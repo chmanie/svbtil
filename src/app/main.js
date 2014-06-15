82149fd4300fbe9020a0f0c5c4aaf6c9f8de0c40
chmanie <chmanie@gmail.com>
2014-06-15 14:44:09 +0200

    animation and styling of landingpage

--- src/app/main.js
+++ src/app/main.js
@@ -3,6 +3,7 @@
   'use strict';
 
   var ANIM_END = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+  var TRANSITION_END = 'transitionend oTransitionEnd webkitTransitionEnd';
 
   var $ = window.jQuery;
 
@@ -14,11 +15,14 @@
   var animate = {
     headerWrapper: function () {
       var headerWrapper = $('#svb-header-wrapper');
-      headerWrapper.addClass('animated fadeIn');
-      headerWrapper
-        .one(ANIM_END, function move () {
-          $(this).addClass('final-position');
-      });
+      headerWrapper.addClass('animated fadeIn')
+        .one('click', function move () {
+          headerWrapper.css('height', '0');
+          $(this).find('h1').addClass('final-position')
+            .one(TRANSITION_END, function () {
+              $('#svb-content-wrapper').addClass('animated fadeIn');
+            });
+        });
     }
   };
 
